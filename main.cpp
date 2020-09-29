84527c545125bee844dd5e9c8297a949b1c40fcf
Miguel Madrigal <miguel.madrigal7676@alumnos.udg.mx>
2020-09-29 12:31:54 -0500

    prueba01

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    string dataType;
+    getline(cin, dataType);
+
+    if(dataType == "char")
+        cout << sizeof(char) << endl;
+    else if(dataType == "int")
+        cout << sizeof(int) << endl;
+    else if(dataType == "short")
+        cout << sizeof(short) << endl;
+    else if(dataType == "float")
+        cout << sizeof(float) << endl;
+    else if(dataType == "double")
+        cout << sizeof(double) << endl;
+    else if(dataType == "long")
+        cout << sizeof(long) << endl;
 
     return 0;
 }
